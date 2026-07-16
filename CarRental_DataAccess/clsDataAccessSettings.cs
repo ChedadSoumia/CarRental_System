79d833605465f41d2544fcdbcc1756899a31820e
ChedadSoumia <chedadsoumia4@gmail.com>
2026-07-16 16:55:53 -0700

    beginning of project

---
+++ CarRental_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=.;Database=P4_CarRental;User Id=sa;Password=sa123456;";
+    }
+}
