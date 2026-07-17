08b27deab485fa61ba9220d45f2290baf2ae9de1
ChedadSoumia <chedadsoumia4@gmail.com>
2026-07-16 17:20:03 -0700

    Add FuelTypes data access and business layer

--- CarRental_DataAccess/clsFuleTypesData.cs
+++ CarRental_DataAccess/clsFuleTypesData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,294 @@ namespace CarRental_DataAccess
 {
     public class clsFuleTypesData
     {
+        public static bool GetFuelTypeByID(int FuelTypeID, ref string FuelTypeName)
+        {
+            bool IsFound = false;
+
+            SqlConnection Connection =
+                new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = @"SELECT *
+                         FROM FuleTypes
+                         WHERE FuelTypeID = @FuelTypeID";
+
+            SqlCommand Command = new SqlCommand(Query, Connection);
+
+            Command.Parameters.AddWithValue("@FuelTypeID", FuelTypeID);
+
+            try
+            {
+                Connection.Open();
+
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                if (Reader.Read())
+                {
+                    IsFound = true;
+
+                    FuelTypeName = (string)Reader["FuelTypeName"];
+                }
+
+                Reader.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                Connection.Close();
+            }
+
+            return IsFound;
+        }
+
+        public static bool GetFuelTypeByName(string FuelTypeName, ref int FuelTypeID)
+        {
+            bool IsFound = false;
+
+            SqlConnection Connection =
+                new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = @"SELECT *
+                         FROM FuleTypes
+                         WHERE FuelTypeName = @FuelTypeName";
+
+            SqlCommand Command = new SqlCommand(Query, Connection);
+
+            Command.Parameters.AddWithValue("@FuelTypeName", FuelTypeName);
+
+            try
+            {
+                Connection.Open();
+
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                if (Reader.Read())
+                {
+                    IsFound = true;
+
+                    FuelTypeID = (int)Reader["FuelTypeID"];
+                }
+
+                Reader.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                Connection.Close();
+            }
+
+            return IsFound;
+        }
+
+        public static DataTable GetAllFuleTypes()
+        {
+            DataTable DT = new DataTable();
+
+            SqlConnection Connection =
+                new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = @"SELECT *
+                         FROM FuleTypes
+                         ORDER BY FuelTypeID";
+
+            SqlCommand Command = new SqlCommand(Query, Connection);
+
+            try
+            {
+                Connection.Open();
+
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                if (Reader.HasRows)
+                    DT.Load(Reader);
+
+                Reader.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                Connection.Close();
+            }
+
+            return DT;
+        }
+
+        public static int AddNewFuelType(string FuelTypeName)
+        {
+            int FuelTypeID = -1;
+
+            SqlConnection Connection =
+                new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = @"
+        INSERT INTO FuleTypes (FuelTypeName)
+        VALUES (@FuelTypeName);
+
+        SELECT SCOPE_IDENTITY();";
+
+            SqlCommand Command = new SqlCommand(Query, Connection);
+
+            Command.Parameters.AddWithValue("@FuelTypeName", FuelTypeName);
+
+            try
+            {
+                Connection.Open();
+
+                object Result = Command.ExecuteScalar();
+
+                if (Result != null &&
+                    int.TryParse(Result.ToString(), out int InsertedID))
+                {
+                    FuelTypeID = InsertedID;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                Connection.Close();
+            }
+
+            return FuelTypeID;
+        }
+
+        public static bool UpdateFuelType(int FuelTypeID, string FuelTypeName)
+        {
+            bool IsUpdated = false;
+
+            SqlConnection Connection =
+                new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = @"UPDATE FuleTypes
+                         SET FuelTypeName = @FuelTypeName
+                         WHERE FuelTypeID = @FuelTypeID";
+
+            SqlCommand Command = new SqlCommand(Query, Connection);
+
+            Command.Parameters.AddWithValue("@FuelTypeID", FuelTypeID);
+            Command.Parameters.AddWithValue("@FuelTypeName", FuelTypeName);
+
+            try
+            {
+                Connection.Open();
+
+                IsUpdated = Command.ExecuteNonQuery() > 0;
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                Connection.Close();
+            }
+
+            return IsUpdated;
+        }
+
+        public static bool DeleteFuelType(int FuelTypeID)
+        {
+            bool IsDeleted = false;
+
+            SqlConnection Connection =
+                new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = @"DELETE FROM FuleTypes
+                         WHERE FuelTypeID = @FuelTypeID";
+
+            SqlCommand Command = new SqlCommand(Query, Connection);
+
+            Command.Parameters.AddWithValue("@FuelTypeID", FuelTypeID);
+
+            try
+            {
+                Connection.Open();
+
+                IsDeleted = Command.ExecuteNonQuery() > 0;
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                Connection.Close();
+            }
+
+            return IsDeleted;
+        }
+
+        public static bool IsFuelTypeExist(int FuelTypeID)
+        {
+            SqlConnection Connection =
+                new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = @"SELECT Found = 1
+                         FROM FuleTypes
+                         WHERE FuelTypeID = @FuelTypeID";
+
+            SqlCommand Command = new SqlCommand(Query, Connection);
+
+            Command.Parameters.AddWithValue("@FuelTypeID", FuelTypeID);
+
+            try
+            {
+                Connection.Open();
+
+                object Result = Command.ExecuteScalar();
+
+                return Result != null;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
+
+
+        public static bool IsFuelTypeExist(string FuelTypeName)
+        {
+            SqlConnection Connection =
+                new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string Query = @"SELECT Found = 1
+                         FROM FuleTypes
+                         WHERE FuelTypeName = @FuelTypeName";
+
+            SqlCommand Command = new SqlCommand(Query, Connection);
+
+            Command.Parameters.AddWithValue("@FuelTypeName", FuelTypeName);
+
+            try
+            {
+                Connection.Open();
+
+                object Result = Command.ExecuteScalar();
+
+                return Result != null;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
     }
 }
--- CarRental_business/clsFuleTypes.cs
+++ CarRental_business/clsFuleTypes.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using CarRental_DataAccess;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,100 @@ namespace CarRental_business
 {
     public class clsFuleTypes
     {
+
+        public enum enMode { eAddNew = 0, eUpdate = 1 };
+        public enMode Mode = enMode.eAddNew;
+
+        public int FuelTypeID { get; set; }
+        public string FuelTypeName { get; set; }
+
+        public clsFuleTypes()
+        {
+            FuelTypeID = -1;
+            FuelTypeName = "";
+            Mode = enMode.eAddNew;
+        }
+
+        private clsFuleTypes(int fuelTypeID, string fuelTypeName)
+        {
+            FuelTypeID = fuelTypeID;
+            FuelTypeName = fuelTypeName;
+
+            Mode = enMode.eUpdate;
+        }
+
+        public static clsFuleTypes Find(int fuelTypeID)
+        {
+            string fuelTypeName = "";
+
+            if (clsFuleTypesData.GetFuelTypeByID(fuelTypeID, ref fuelTypeName))
+            {
+                return new clsFuleTypes(fuelTypeID, fuelTypeName);
+            }
+
+            return null;
+        }
+
+        public static clsFuleTypes Find(string fuelTypeName)
+        {
+            int fuelTypeID = -1;
+
+            if (clsFuleTypesData.GetFuelTypeByName(fuelTypeName, ref fuelTypeID))
+            {
+                return new clsFuleTypes(fuelTypeID, fuelTypeName);
+            }
+
+            return null;
+        }
+
+        public static bool IsFuelTypeExist(int fuelTypeID)
+        {
+            return clsFuleTypesData.IsFuelTypeExist(fuelTypeID);
+        }
+        public static bool IsFuelTypeExist(string fuelTypeName)
+        {
+            return clsFuleTypesData.IsFuelTypeExist(fuelTypeName);
+        }
+
+        private bool _AddNewFuleType()
+        {
+            FuelTypeID = clsFuleTypesData.AddNewFuelType(this.FuelTypeName);
+            return (FuelTypeID != -1);
+        }
+
+        private bool _UpdateFuleType()
+        {
+            return clsFuleTypesData.UpdateFuelType(this.FuelTypeID, this.FuelTypeName);
+        }
+
+        public static DataTable GetAllFuelTypes()
+        {
+            return clsFuleTypesData.GetAllFuleTypes();
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.eAddNew:
+                    if (_AddNewFuleType())
+                    {
+
+                        Mode = enMode.eUpdate;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.eUpdate:
+
+                    return _UpdateFuleType();
+
+            }
+
+            return false;
+        }
     }
 }
