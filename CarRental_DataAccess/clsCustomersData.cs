9aa23ecf7344408efa293fd6940cca3f8f7708f1
ChedadSoumia <chedadsoumia4@gmail.com>
2026-09-24 14:02:35 -0700

    Add Customers Classes Bussiness Layer and DataAccess Layer

---
+++ CarRental_DataAccess/clsCustomersData.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental_DataAccess
+{
+    public class clsCustomersData
+    {
+        public static bool GetCustomerByID(int CustomerID, ref int PersonID,ref string NationalNo,ref string DriverLicenseNumber)
+        {
+            bool isFound = false;
+
+            try
+            {
+                using (SqlConnection connection =
+                       new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_GetCustomerByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@CustomerId", CustomerID);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Process results
+                            while (reader.Read())
+                            {
+                                isFound = true;
+                                PersonID = (int)reader["PersonID"];
+                                NationalNo = (string)reader["NationalNo"];
+                                DriverLicenseNumber = (string)reader["DriverLicenseNumber"];
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                clsEventLog.LogError("",ex);
+                return false;
+            }
+
+            return isFound;
+        }
+        public static bool GetCustomerByNationalNo(ref int CustomerID, ref int PersonID, string NationalNo, ref string DriverLicenseNumber)
+        {
+            bool isFound = false;
+
+            try
+            {
+                using (SqlConnection connection =
+                       new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_GetCustomerInfoByNationalNo", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@National_no", CustomerID);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Process results
+                            while (reader.Read())
+                            {
+                                isFound = true;
+                                CustomerID = (int)reader["CustomerID"];
+                                PersonID = (int)reader["PersonID"];
+                                DriverLicenseNumber = (string)reader["DriverLicenseNumber"];
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                clsEventLog.LogError("", ex);
+                return false;
+            }
+
+            return isFound;
+        }
+
+
+        public static DataTable GetAllCustomers()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                using (SqlConnection connection =
+                       new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_GetAllCustomer", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            dt.Load(reader);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+            }
+
+            return dt;
+        }
+
+        public static int AddNewCustomer(int PersonID, string NationalNo, string DriverLicenseNumber)
+        {
+            int CustomerID = -1;
+
+            try
+            {
+                using (SqlConnection connection =
+                       new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_AddNewCustomer", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@Person_id", PersonID);
+                        command.Parameters.AddWithValue("@National_no", NationalNo);
+                        command.Parameters.AddWithValue("@Driver_license_number", DriverLicenseNumber);
+
+                        SqlParameter outputIdParam = new SqlParameter("@CustomerID", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+                        command.Parameters.Add(outputIdParam);
+
+                        command.ExecuteNonQuery();
+
+                        CustomerID = (int)command.Parameters["@CustomerID"].Value;
+                        connection.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                clsEventLog.LogError("", ex);
+            }
+
+
+
+
+            return CustomerID;
+        }
+
+        public static bool UpdateCustomer(int CustomerID, int PersonID, string NationalNo, string DriverLicenseNumber)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_UpdateCustomer", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@CustomerID", CustomerID);
+                        command.Parameters.AddWithValue("@@Person_id", PersonID);
+                        command.Parameters.AddWithValue("@National_no", NationalNo);
+                        command.Parameters.AddWithValue("@Driver_license_number", DriverLicenseNumber);
+
+
+                        rowsAffected = command.ExecuteNonQuery();
+
+                    }
+                }
+            }
+            catch (Exception ex) {
+                clsEventLog.LogError("", ex);
+                return false; 
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        public static bool IsCustomerExist(int CustomerID)
+        {
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_CheckCustomerExistsByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@CustomerID", CustomerID);
+
+                        SqlParameter returnParameter = new SqlParameter("@ReturnVal", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.ReturnValue
+                        };
+
+                        command.Parameters.Add(returnParameter);
+                        command.ExecuteNonQuery();
+
+                        int result = (int)returnParameter.Value;
+
+
+                        return (result == 1);
+                    }
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteCustomer(int CustomerID)
+        {
+            int rowsAffected = 0;
+            try
+            {
+                using (SqlConnection connection =
+                       new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_DeleteCustomer", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@CustomerID", CustomerID);
+
+
+                        rowsAffected = command.ExecuteNonQuery();
+
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                clsEventLog.LogError("", ex);
+                return false;
+            }
+            return (rowsAffected > 0);
+
+        }
+
+    }
+}
---
+++ CarRental_business/clsCustomers.cs
@@ -0,0 +1,117 @@
+﻿using CarRental_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental_business
+{
+    public class clsCustomers
+    {
+
+        public enum enMode { eAddNew = 1, eUpdate = 2 }
+        public enMode Mode = enMode.eAddNew;
+
+        public int CustomerID { get; set; }
+        public int PersonID { get; set; }
+        public clsPerson PersonInfo;
+        public string NationalNo { get; set; }
+        public string DriverLicenseNumber { get; set; }
+
+
+        public clsCustomers()
+        {
+            this.CustomerID = -1;
+            this.PersonID = -1;
+            this.NationalNo = "";
+            this.DriverLicenseNumber = "";
+            Mode = enMode.eAddNew;
+        }
+        private clsCustomers(int customerID, int personID, string nationalNo, string driverLicenseNumber)
+        {
+            this.CustomerID = customerID;
+            this.PersonID = personID;
+            this.PersonInfo = clsPerson.Find(personID);
+            this.NationalNo = nationalNo;
+            this.DriverLicenseNumber = driverLicenseNumber;
+            Mode = enMode.eUpdate;
+        }
+        private bool _AddNewCustomer()
+        {
+            this.CustomerID = clsCustomersData.AddNewCustomer(this.PersonID, this.NationalNo, this.DriverLicenseNumber);
+            return (this.CustomerID != -1);
+        }
+        private bool _UpdateCustomer()
+        {
+
+            return clsCustomersData.UpdateCustomer(this.CustomerID, this.PersonID, this.NationalNo, this.DriverLicenseNumber);
+        }
+        public static clsCustomers Find(int customerID)
+        {
+            int customerId = -1, PersonId = -1;
+            string nationalNo = "", driverLicenseNumber = "";
+            bool isFound = clsCustomersData.GetCustomerByID(customerID, ref PersonId, ref nationalNo, ref driverLicenseNumber);
+            if (isFound)
+            {
+                return new clsCustomers(customerId, PersonId, nationalNo, driverLicenseNumber);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        public static clsCustomers Find(string nationalNo)
+        {
+            int customerID = -1,customerId = -1, PersonId = -1;
+            string driverLicenseNumber = "";
+            bool isFound = clsCustomersData.GetCustomerByNationalNo(ref customerID, ref PersonId, nationalNo, ref driverLicenseNumber);
+            if (isFound)
+            {
+                return new clsCustomers(customerId, PersonId, nationalNo, driverLicenseNumber);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public static DataTable GetAllCustomers()
+        {
+            return clsCustomersData.GetAllCustomers();
+        }
+        public static bool DeleteCustomer(int customerID)
+        {
+            return clsCustomersData.DeleteCustomer(customerID);
+        }
+
+        
+        public static bool IsCustomerExistByID(int CustomerID)
+        {
+            return clsCustomersData.IsCustomerExist(CustomerID);
+        }
+        
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.eAddNew:
+                    if(_AddNewCustomer())
+                    {
+                        Mode = enMode.eUpdate;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case enMode.eUpdate:
+                    return _UpdateCustomer();
+            }
+            return false;
+        }
+    }
+}
